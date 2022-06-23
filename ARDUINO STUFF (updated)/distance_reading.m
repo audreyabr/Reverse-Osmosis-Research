ad529198fd2a3498a16102d93ceac9a03c129dc8
Bahar Maghbouleh <bmaghbouleh@olin.edu>
2022-06-23 14:43:18 -0400

    Update distance_reading.m
    changed to take "repeat" number of measurements every time distance_reading is called in hopes of better data collection

--- ARDUINO STUFF (updated)/distance_reading.m
+++ ARDUINO STUFF (updated)/distance_reading.m
@@ -9,7 +9,11 @@ function [distance_list, distance]  = distance_reading(arduino_object, ultrasoni
 % Returns: distance_list - the list of distance readings (cm)
 
     %ultrasonicObj = ultrasonic(arduino_object, triggerPin, echoPin, 'OutputFormat','double')
-    distance = readDistance(ultrasonicObj) *100 %cm, display distance
+    repeat = 5;
+    for i = 1:repeat
+        distance_many(1,i) = readDistance(ultrasonicObj) *100; %cm, display distance
+    end
+    distance = sum(distance_many)/repeat
     distance_list(end+1, 1) = distance;
     
 end
