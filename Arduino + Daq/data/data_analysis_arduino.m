27ceb525f5d1dcf4d76145d8aee334ad0b824fe6
audreyabr <78472378+audreyabr@users.noreply.github.com>
2022-07-08 15:07:02 -0400

    Update data_analysis_arduino.m

--- Arduino + Daq/data/data_analysis_arduino.m
+++ Arduino + Daq/data/data_analysis_arduino.m
@@ -1,11 +1,11 @@
 
 % assigning variables
-time = time_list(1:138,:);                              % time, seconds
+time = time_list(1:end-1,:);                              % time, seconds
 conductivity = conductivity_list(:,1);                  % conductivity, mS/cm
-distance = distance_list(1:138,:);                      % distance, cm
-batch_flow_rate = flowrate_list(1:138,:);               % flow rate, mL/min
-permeate_flow_rate = permeate_flowrate_list(1:138,:);   % flow rate, mL/min
-mass = mass_list(1:138,:);                              % mass, g
+distance = distance_list(1:end-1,:);                      % distance, cm
+batch_flow_rate = flowrate_list(1:end-1,:);               % flow rate, mL/min
+permeate_flow_rate = permeate_flowrate_list_D(1:end-1,:);   % flow rate, mL/min
+mass = mass_list(1:end-1,:);                              % mass, g
 
 % calculates and plots salinity ,flux, and permeability
 % parameters to enter each time
