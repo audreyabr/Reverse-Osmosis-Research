4536c029131dab163cdb33a513ccac593643416a
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-07-14 16:44:42 -0400

    Change state list in main

--- Arduino + Daq/salinity_simulator.m
+++ Arduino + Daq/salinity_simulator.m
@@ -1,26 +1,26 @@
-function [brine_salinity,brine_conductivity,batch_salinity,batch_conductivity] = salinity_simulator(salt_name)
+function [brine_salinity,brine_conductivity,batch_salinity,batch_conductivity] = salinity_simulator()
 
 % input theoratical batch distances, salinity, and brine volume
-empty_dis = input("empty distance: "); % cm
 full_dis = input("full distance: "); % cm
+empty_dis = input("empty distance: "); % cm
 batch_salinity = input("Feed salinity(mM): ");
-brine_vol = input("brine volume: "); % ml
 
 % constants
 tank_height = 29.845; % cm
 
-if salt_name == "NaCl"||salt_name == "nacl"||salt_name == "sodium chloride"
-    cond_at_1pct = 17.6;    % conductivity in mS/cm for 1% NaCl
-    molar_mass = 58.44; % g/mol
-end
+% if salt_name == "NaCl"||salt_name == "nacl"||salt_name == "sodium chloride"
+%     cond_at_1pct = 17.6;    % conductivity in mS/cm for 1% NaCl
+%     molar_mass = 58.44; % g/mol
+% end
 
-if salt_name == "CaSO4"||salt_name == "caso4"||salt_name == "calcium sulfate"
+% if salt_name == "CaSO4"||salt_name == "caso4"||salt_name == "calcium sulfate"
     cond_at_1pct = 22.8;    % measured conductivity in mS/cm for 1% NaCl
     molar_mass = 147.01; % g/mol
-end
+% end
 
 % calculation
 batch_vol = Water_Tank_Calculations(tank_height - full_dis) - Water_Tank_Calculations(tank_height - empty_dis); % ml
+brine_vol = Water_Tank_Calculations(tank_height - empty_dis) + 100;
 RR = (batch_vol - brine_vol) / batch_vol;
 
 brine_salinity = batch_salinity/(1-RR); % milimolar - 1/1000mol/L (RR: recovery rate)
@@ -30,7 +30,12 @@ brine_conductivity = brine_salinity_pct * cond_at_1pct;
 batch_salinity_pct = 100 * (((batch_salinity / 1000) * molar_mass) / 1000); % in percent
 batch_conductivity = batch_salinity_pct * cond_at_1pct; % mS
 
+[grams_CaCl_dihydrate,grams_Na2SO4]= CaSO4_mixing(batch_salinity,batch_vol/1000);% convert batch volume to liter
+
 % output brine salinity and conductivity range
+disp("batch volume(mL): " + batch_vol)
+disp("Add CaCl2: " + grams_CaCl_dihydrate)
+disp("Add Na2SO4: " + grams_Na2SO4)
 disp("batch conductivity(mS): " + batch_conductivity)
 disp("brine conductivity(mS): " + brine_conductivity)
 disp("salinity range of this batch(mM): " + batch_salinity + " to " + brine_salinity)
