0af377909e6dd3653ff3a17141346c793a7bae9f
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-07-20 09:04:52 -0400

    Add previous data graphs and fix simulator

--- Arduino + Daq/salinity_simulator.m
+++ Arduino + Daq/salinity_simulator.m
@@ -1,27 +1,26 @@
-function [dist_full,dist_empty,batch_vol,brine_vol,brine_concn,min_feed_pres,batch_conductivity,brine_conductivity,grams_CaCl_dihydrate,grams_Na2SO4] = salinity_simulator(feed_concn,batch_time,RR)
+function [dist_full,dist_empty,batch_vol,brine_vol,brine_concn,min_feed_pres,batch_conductivity,brine_conductivity,grams_CaCl_dihydrate,grams_Na2SO4] = salinity_simulator(feed_concn,batch_time,RR,avg_flux)
 % calculate batch volume(L), distance of full and empty(cm), minimal feed
 % pressure(psi), brine conductvity(mS), Na2SO4 and CaCl2-H2O needed(g)
-% based on intended feed concentration(mM), batch time(hour) and recovery
-% rate(%)
+% based on intended feed concentration(mM), batch time(hour), avrage flux 
+% and recovery rate(%)
 
 % constants
 membrane_area = 0.0238; % m^2 (SW measurement feed side, 2019 module)
 T = 25 + 273.15; % Kelvin, tempareture of water
-flush_tube_volume = 0.170; % L, estimated with current tubing loop
+flush_tube_volume = 0.10; % L, estimated with current tubing loop (remeasure!)
 Kw= 3.39755; %L/m2.h.bar, calculated with BW30-4040 membrane
 unit_condu = 0.495;    % mS/cm, conductivity of 1mM of CaSO4 and 2mM of NaCl
 
 % calculate for minimal hydraulic pressure
 brine_concn = feed_concn / (1 - RR);% mM,concentration of CaSO4 in brine
 feed_osmotic = 8.314 * T * 6 * feed_concn / 1000;% kPa, feed osmotic pressure calculated by van't Hoff equation
-feed_membrane_osmotic = 1.1 * feed_osmotic; % kPa, feed osmotic pressure at membrane
+feed_membrane_osmotic = 0.1450 * 1.1 * feed_osmotic; % psi, feed osmotic pressure at membrane converted to psi
 brine_osmotic =  8.314 * T * 6 * brine_concn / 1000;% kPa, brine osmotic pressure
 brine_membrane_osmotic = 0.1450 * 1.1 * brine_osmotic; % psi, brine osmotic pressure at membrane converted to psi
-min_feed_pres = 400; % psi, minimal hydraulic pressure needed
 
 % calculate for batch volume
-avg_osmotic =(feed_membrane_osmotic + brine_membrane_osmotic) / 2; % kPa
-avg_flux = (min_feed_pres * 0.06895 - avg_osmotic * 0.01) * Kw; % L/m2.h, converted pressure to bar
+avg_osmotic =(feed_membrane_osmotic + brine_membrane_osmotic) / 2; % psi
+min_feed_pres = avg_flux / Kw / 0.06895 + avg_osmotic  ; %psi, minimal hydraulic pressure needed
 avg_flowrate = avg_flux * membrane_area; % L/h
 batch_vol = batch_time * avg_flowrate; % L
 brine_vol = batch_vol * (1 - RR); % L
