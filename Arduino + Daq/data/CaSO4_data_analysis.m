737fdd63b73243178ad4e51ae091eb6b0547108f
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-07-21 10:06:40 -0400

    Add new data analysis file and test four data graph

--- Arduino + Daq/data/CaSO4_data_analysis.m
+++ Arduino + Daq/data/CaSO4_data_analysis.m
@@ -1,36 +1,40 @@
+D = [length(time_list),length(conductivity_list),length(distance_list),length(flowrate_list),length(permeate_flowrate_list),length(permeate_volume_list)];
+data_length = min(D,[],"all");
 
 % assigning variables
-time = time_list;                              % time, seconds
-conductivity = conductivity_list;                  % conductivity, mS/cm
-distance = distance_list(1:end-1);                      % distance, cm
-batch_flow_rate = flowrate_list;               % flow rate, mL/min
-permeate_flow_rate = permeate_flowrate_list;   % flow rate, mL/min
-mass = permeate_volume_list;                              % mass, g
+time = time_list(1:data_length);                                  % time, seconds
+conductivity = conductivity_list(1:data_length);                  % conductivity, mS/cm
+distance = distance_list(1:data_length);                          % distance, cm
+batch_flow_rate = flowrate_list(1:data_length);                   % flow rate, mL/min
+permeate_flow_rate = permeate_flowrate_list(1:data_length);       % flow rate, mL/min
+mass = permeate_volume_list(1:data_length);                       % mass, g
 
 % calculates and plots salinity ,flux, and permeability
 % parameters to enter each time
-sal_pct_i = 0.5;       % initial salinity (in %) for salinity-based estimation of recovery
-P_psi = 375;            % applied pressure in psi
-permeate_cond = 1.8;  % conductivity of permeate in mS/cm 
+sal_mM_i = 4.5;       % mM, initial salinityfor salinity-based estimation of recovery
+P_psi = 575;          % applied pressure in psi
+permeate_cond = 0.107;  % conductivity of permeate in mS/cm 
 
 % preset parameters
 t_min_av = 0.5;         % minutes to average over
-cond_at_1pct = 17.6;    % conductivity in mS/cm for 1% NaCl
-pi_at_1pct = 7.9566;    % osmotic pressure in bar for 1% NaCl
-t_interval = 1;         % s NOTE: data time interval is approximate
+condu_at_1mM = 0.495;   % mS/cm, conductivity of 1mM of CaSO4 and 2mM of NaCl
+pi_at_1mM = 14.8729;    % psi, osmotic pressure of 1mM of CaSO4 and 2mM of NaCl
+t_interval = 1;         % NOTE: data time interval is approximate
+A_m = 0.0238;           % m^2, membrane area(SW measurement feed side, 2019 module)
 
 close all
-sal_pct = conductivity / cond_at_1pct;  % salinity in % by wt (where linear!)
+sal_mM = conductivity / condu_at_1mM;  % mM, salinity calculated by linear relation to conductivity
 n_av = t_min_av * 60/t_interval;        % number of points to make flux avg across
 
-sal_pct_av = movmean(sal_pct, n_av);
+sal_mM_av = movmean(sal_mM, n_av);
 flowrate_av = (mass(n_av+1:end) - mass(1:end-n_av)) ./ (time(n_av+1:end) - time(1:end-n_av));
-A_m = 0.0238; % m^2 (SW measurement feed side, 2019 module)
-flux_lmh = flowrate_av / 1000*3600 / A_m;
-RR_i_cond = 1 - sal_pct_i ./ sal_pct_av; % conductivity-based instantaneous RR assuming no salt permeation!
+
+flux_lmh = flowrate_av / 1000*3600 / A_m; % L/m2.h
+RR_i_cond = 1 - sal_mM_i ./ sal_mM_av; % conductivity-based instantaneous RR assuming no salt permeation!
 P_bar = P_psi * 0.0689;
-pi_bar = pi_at_1pct * sal_pct_av; % est. osmotic pressure in bar
-perm_LMHB = flux_lmh ./ (P_bar-pi_bar(1:end-n_av)); % permeability
+pi_psi = pi_at_1mM * sal_mM_av; % est. osmotic pressure in psi
+pi_bar = pi_psi * 0.0689;
+perm_LMHB = flux_lmh ./ (P_bar-pi_bar(1:end-n_av)); %LMH/bar, permeability
 
 
 % plots conductivity over time
@@ -89,7 +93,7 @@ ylim([0,200])
 
 % plots salinity
 figure
-plot(time, sal_pct_av,'o')
+plot(time, sal_mM_av,'o')
 title("Salinity Over Time")
 xlabel('Time (s)')
 ylabel('Salinity (% NaCl)')
