56938f60d8f019b1d1ec24c0c4130cca61a42d3d
Diana-GA <98130651+Diana-GA@users.noreply.github.com>
2022-07-21 20:53:08 -0400

    testing squishy processing file
    new file made - ..._babysteps. has been able to produce plots using data from 7/20 squishy run

--- Arduino + Daq/squishy_processing.m
+++ Arduino + Daq/squishy_processing.m
@@ -1,5 +1,5 @@
-function [salt_rej, mem_CaSO4_conc, conc_polar,SI_gypsum, ind_time, nucl_prob]...
-    = squishy_processing(init_conc,feed_flow,perm_flow,feed_cond,perm_cond,timestep)
+function [salt_rej, mem_CaSO4_conc, conc_polar,SI_gypsum, ind_time, C_t]...
+    = squishy_processing(init_conc,feed_flow,perm_flow,feed_cond,perm_cond,time_list)
 %by Diana last upload to google drive 7/20/22 around 4:55pm pacific
 % next things to do
 % - find a way to integrate the time step
@@ -19,16 +19,18 @@ function [salt_rej, mem_CaSO4_conc, conc_polar,SI_gypsum, ind_time, nucl_prob]..
 % time = time_list(1:end-1,:);  this came from data_analysis_arduino file
 % what does it mean?
 
+
 % initializing variables
 mem_const = 0.99 % membrane constant (K)
+perm_cond = 0.5 % permeate conductivity (mS)
 
 % Calculating Salt Rejection
-salt_rej = [1-(perm_cond)/mean(feed_cond)] % salt rejection
+salt_rej = (1-(perm_cond))./mean(feed_cond) % salt rejection
 
 % Calculating CaSO4 Concentration at the membrane
-recovery = (perm_flow/feed_flow)*100  % expressed in terms of percentage
+recovery = (perm_flow./feed_flow)*100  % expressed in terms of percentage
 mem_CaSO4_conc = (1/(1-recovery)) * init_conc % membrane CaSO4 concentration
-conc_polar = mem_const * exp(perm_flow/mean(feed_flow)) % concentration polarization
+conc_polar = mem_const * exp(perm_flow./mean(feed_flow)) % concentration polarization
 
 % Calculating Saturation Index of Gypsum
 SI_gypsum = 0.527 * log(init_conc) - 1.5073 
@@ -37,9 +39,16 @@ SI_gypsum = 0.527 * log(init_conc) - 1.5073
 ind_time = 1.66*(10^6)*(exp(-0.174*init_conc)) % Induction Time in seconds (s)
 
 % Integrating the Induction Rate over all timepsteps
-C_t = mem_CaSO4_conc./timestep    % concentration as a funciton of time
-% need the figure this out better, might not be it
-nucl_prob = int(1/(ind_time.*C_t),0,timestep)  % nucleation probability
+C_t = mem_CaSO4_conc/time_list    % concentration as a funciton of time
+% need the figure this out better in terms of time
+
+%should this be left outside the function
+%syms ind_time C_t
+%f(ind_time,C_t) = 1/(ind_time*C_t)
+%fint = int(f,ind_time,C_t)
+%nucl_prob = int(1/(ind_time*C_t),time_list,0,500)  % nucleation probability
 % need to figure out the integrating over the timesteps
 % and how to use the int for integration function in matlab
 end
+
+
--- testing_squishy_processing.m
+++ testing_squishy_processing.m
@@ -1,14 +1,23 @@
 % Testing squishy_processing indealing by running a loop
 % Simulates a squishy batch
 
-timestep = 30;         %total number of seconds to simulate
+% creating out lists to store data from loop
+salt_rej_list = [];
+mem_CaSO4_conc_list = [];
+conc_polar_list = [];
+SI_gypsum_list = [];
+ind_time_list = [];
+C_t_list = [];
 
+for i = 1:time_list %only needs to run n-1 times to collect data through second n
+    squishy_processing(initial_conductivity,flowrate_list,permeate_flowrate_list,conductivity_list,conductivity,time_list)
+    salt_rej_list(end+1) = salt_rej;
+    mem_CaSO4_conc_list(end+1) = mem_CaSO4_conc;
+    conc_polar_list(end+1) = conc_polar;
+    SI_gypsum_list(end+1) = SI_gypsum;
+    ind_time_list(end+1) = ind_time;
+    C_t_list(end+1) = C_t
+end   
 
-for i = 1:(timestep-1) %only needs to run n-1 times to collect data through second n
-    init_conc = 150;
-    feed_flow = 200;
-    perm_flow = 10;
-    feed_cond = 50;
-    perm_cond = 7;
-    squishy_processing(init_conc, feed_flow, perm_flow, feed_flow, perm_cond,timestep)
-end    
+f = @(ind_time_list,C_t_list) 1./(ind_time_list.*C_t_list);
+nucl_prob = integral2(f,0,500,0,500);
