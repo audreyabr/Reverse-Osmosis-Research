ee5b99aa0456e9c59f81fd557d1e91696064084c
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-11-20 15:01:17 -0500

    Add pressure sensor - daq  test file

---
+++ Arduino + Daq/test files/test_pressure_sensor.m
@@ -0,0 +1,26 @@
+% set up DAQ
+daqreset
+d = daqlist;
+dq = daq("ni");
+Daqtype = d.DeviceID;
+
+ch00ai = addinput(dq,Daqtype,'ai0','Voltage');  % permeate flowrate in Channel AI0(+)
+ch00ai.TerminalConfig = 'Differential';
+ch01ai = addinput(dq,Daqtype,'ai1','Voltage');  % batch flowrate in Channel AI1(+)
+ch01ai.TerminalConfig = 'Differential';
+ch02ai = addinput(dq,Daqtype,'ai2','Voltage');  % conductivity in Channel AI2
+ch02ai.TerminalConfig = 'Differential';
+ch03ai = addinput(dq,Daqtype,'ai3','Voltage');  % pressure in Channel AI3
+ch03ai.TerminalConfig = 'Differential';
+
+% Initialize 
+index = 25;
+voltage_list = []; 
+
+daqName = dq;
+% taking multiple readings from each input pin
+for i = 1:index
+    daqName.Rate = 5000;
+    voltage_list(i,1:4) = read(daqName, "OutputFormat", "Matrix");
+end 
+disp(voltage_list);
